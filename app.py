c5559cc91e403e969c6299f865f0c4a382eecb4d
zhikaichen99 <zhikaichen1999@gmail.com>
2023-02-24 12:49:03 -0500

    script for the flask app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pandas as pd
+from utils.scraper import scrape_data
+from utils.probability import player_over_probability
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    player_name = request.form['player_name']
+    threshold = int(request.form['threshold'])
+    stat = request.form['stat']
+
+    df = scrape_data(player_name)
+    probability = player_over_probability(stat, threshold, df)
+
+    return render_template('index.html', prediction_text='The probability of {} getting over {} {} is {:.2f}%'.format(player_name, threshold, stat, probability*100))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
