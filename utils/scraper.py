fa11edc9fb4c248b997d77dc9732ec76a8d7d022
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-10 17:42:18 -0500

    scraping function for ncaab

--- utils/scraper.py
+++ utils/scraper.py
@@ -37,3 +37,39 @@ def scrape_data_nba(player_name, last_n_games = None):
 
     return df[:last_n_games]
 
+def scrape_data_ncaab(player_name, last_n_games = None):
+
+    url = 'https://www.foxsports.com/college-basketball/{}-player-game-log'.format(player_name)
+
+    # send a request to the url and get response
+    response = requests.get(url)
+
+    # parse content
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # find table element 
+    table = soup.find('table', {'class': 'data-table'})
+
+    # retrieve headers of the table
+    headers = table.findAll('th', {'class': 'ff-n cell-number pointer'})
+    columns = []
+    for header in headers:
+        columns.append(header.text.strip())
+
+    # retrieve row data from the table
+    data = soup.find('tbody', {'class': 'row-data lh-1pt43 fs-14'})
+    rows = data.findAll('tr')[0:]
+    data = []
+    for row in rows:
+        row_data = []
+        for td in row.findAll('td'):
+            row_data.append(td.text.strip())
+        data.append(row_data[2:])
+
+    df = pd.DataFrame(data, columns = columns)
+    df['3FG'] = df['3FG'].str.split('/').str[0]
+
+    print(df)
+
+    return df[:last_n_games]
+
