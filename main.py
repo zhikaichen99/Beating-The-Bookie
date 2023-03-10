2aa2fc733fde38686f2971478c1edc5313de5f9d
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-10 18:09:51 -0500

    added try and error

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ PLAYERS_LIST = ['anthony-edwards']
 NCAA_PLAYERS_LIST = ['andrew-funk', 'seth-lundy', 'jalen-pickett', 'camren-wynter',
                      'chase-audige', 'ty-berry', 'boo-buie', 'matthew-nicholson',
                      'taylor-hendricks', 'ithiel-horton', 'darius-johnson', 'cj-kelly',
-                     'kendric-davis', 'elijah-mccadden', 'deandre-willians', 'mark-mitchell',
+                     'kendric-davis', 'elijah-mccadden',
                      'tyrese-proctor', 'jeremy-roach', 'jordan-miller', 'norchard-omier',
                      'nijel-pack', 'wooga-poplar', 'isaiah-wong', 'jaren-holmes', 'gabe-kalscheur',
                      'tre-king', 'gradey-dick', 'jalen-wilson', 'isiah-dasher', 'gabe-mcglothan',
@@ -32,32 +32,37 @@ NCAA_PLAYERS_LIST = ['andrew-funk', 'seth-lundy', 'jalen-pickett', 'camren-wynte
 def nba():
     df = pd.DataFrame()
     for player_name in PLAYERS_LIST:
-        player_df = scrape_data_nba(player_name, LAST_N_GAMES)
-        player_df = player_df.astype({'PTS': int, 'AST': int, 'REB': int, '3FG': int})
-        player_name = player_name.replace('-', ' ').title()
-        print(f"Created DataFrame for {player_name}")
+        try:
+            player_df = scrape_data_nba(player_name, LAST_N_GAMES)
+            player_df = player_df.astype({'PTS': int, 'AST': int, 'REB': int, '3FG': int})
+            player_name = player_name.replace('-', ' ').title()
+            print(f"Created DataFrame for {player_name}")
 
-        # Looping over each category and threshold to calculate the probability
-        for category, thresholds in zip(['PTS', 'AST', 'REB', '3FG'],
-                                        [POINTS_THRESHOLDS, ASSISTS_THRESHOLDS, REBOUNDS_THRESHOLDS, THREES_THRESHOLDS]):
-            player_list, prop_list, probability_list, last_25_list, average_list = [], [], [], [], []
-            for threshold in thresholds:
-                probability = player_over_probability(category, threshold, player_df)
-                last_25 = player_df[player_df[category] >= threshold]
-                count_last_25 = last_25.shape[0]
-                average = player_df[category].mean()
+            # Looping over each category and threshold to calculate the probability
+            for category, thresholds in zip(['PTS', 'AST', 'REB', '3FG'],
+                                            [POINTS_THRESHOLDS, ASSISTS_THRESHOLDS, REBOUNDS_THRESHOLDS, THREES_THRESHOLDS]):
+                player_list, prop_list, probability_list, last_25_list, average_list = [], [], [], [], []
+                for threshold in thresholds:
+                    probability = player_over_probability(category, threshold, player_df)
+                    last_25 = player_df[player_df[category] >= threshold]
+                    count_last_25 = last_25.shape[0]
+                    average = player_df[category].mean()
 
-                # Checking if the probability of going over the threshold is higher than 80%
-                if (0.80 <= probability <= 0.95):
-                    player_list.append(player_name)
-                    prop_list.append(f"{threshold} {category}")
-                    probability_list.append(probability)
-                    last_25_list.append(count_last_25)
-                    average_list.append(average)
-            data = {'Player': player_list, 'Prop': prop_list, 'Probability': probability_list,
-                    'Last_25': last_25_list, 'Average_Last_25': average_list}
-            df_results = pd.DataFrame(data)
-            df = pd.concat([df, df_results])
+                    # Checking if the probability of going over the threshold is higher than 80%
+                    if (0.80 <= probability <= 0.95):
+                        player_list.append(player_name)
+                        prop_list.append(f"{threshold} {category}")
+                        probability_list.append(probability)
+                        last_25_list.append(count_last_25)
+                        average_list.append(average)
+                data = {'Player': player_list, 'Prop': prop_list, 'Probability': probability_list,
+                        'Last_25': last_25_list, 'Average_Last_25': average_list}
+                df_results = pd.DataFrame(data)
+                df = pd.concat([df, df_results])
+        except Exception as e:
+            # print the error message and continue to the next item
+            print("Error for : {}".format(player_name))
+            continue
 
     df.reset_index(drop=True, inplace=True)
 
@@ -68,33 +73,38 @@ def nba():
 def ncaab():
     df = pd.DataFrame()
     for player_name in NCAA_PLAYERS_LIST:
-        player_df = scrape_data_ncaab(player_name, LAST_N_GAMES)
-        player_df = player_df.astype({'PTS': int, 'AST': int, 'REB': int})
-        player_name = player_name.replace('-', ' ').title()
-        print(f"Created DataFrame for {player_name}")
+        try:
+            player_df = scrape_data_ncaab(player_name, LAST_N_GAMES)
+            player_df = player_df.astype({'PTS': int, 'AST': int, 'REB': int})
+            player_name = player_name.replace('-', ' ').title()
+            print(f"Created DataFrame for {player_name}")
 
-        # Looping over each category and threshold to calculate the probability
-        for category, thresholds in zip(['PTS', 'AST', 'REB'],
-                                        [POINTS_THRESHOLDS, ASSISTS_THRESHOLDS, REBOUNDS_THRESHOLDS]):
-            player_list, prop_list, probability_list, last_25_list, average_list = [], [], [], [], []
-            for threshold in thresholds:
-                probability = player_over_probability(category, threshold, player_df)
-                last_25 = player_df[player_df[category] >= threshold]
-                count_last_25 = last_25.shape[0]
-                average = player_df[category].mean()
+            # Looping over each category and threshold to calculate the probability
+            for category, thresholds in zip(['PTS', 'AST', 'REB'],
+                                            [POINTS_THRESHOLDS, ASSISTS_THRESHOLDS, REBOUNDS_THRESHOLDS]):
+                player_list, prop_list, probability_list, last_25_list, average_list = [], [], [], [], []
+                for threshold in thresholds:
+                    probability = player_over_probability(category, threshold, player_df)
+                    last_25 = player_df[player_df[category] >= threshold]
+                    count_last_25 = last_25.shape[0]
+                    average = player_df[category].mean()
 
-                # Checking if the probability of going over the threshold is higher than 80%
-                if (0.80 <= probability <= 0.95):
-                    player_list.append(player_name)
-                    prop_list.append(f"{threshold} {category}")
-                    probability_list.append(probability)
-                    last_25_list.append(count_last_25)
-                    average_list.append(average)
-            data = {'Player': player_list, 'Prop': prop_list, 'Probability': probability_list,
-                    'Last_25': last_25_list, 'Average_Last_25': average_list}
-            df_results = pd.DataFrame(data)
-            df = pd.concat([df, df_results])
-
+                    # Checking if the probability of going over the threshold is higher than 80%
+                    if (0.80 <= probability <= 0.95):
+                        player_list.append(player_name)
+                        prop_list.append(f"{threshold} {category}")
+                        probability_list.append(probability)
+                        last_25_list.append(count_last_25)
+                        average_list.append(average)
+                data = {'Player': player_list, 'Prop': prop_list, 'Probability': probability_list,
+                        'Last_25': last_25_list, 'Average_Last_25': average_list}
+                df_results = pd.DataFrame(data)
+                df = pd.concat([df, df_results])
+        except Exception as e: 
+            # print the error message and continue to the next item
+            print("Error for : {}".format(player_name))
+            continue
+    
     df.reset_index(drop=True, inplace=True)
 
     # Saving the results to a csv file with today's date
