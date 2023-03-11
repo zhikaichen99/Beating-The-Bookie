419f5f5d475b41005474830afda458fdca5a00e5
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-10 20:20:49 -0500

    streamlit app

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from utils.scraper import scrape_data
+from utils.scraper import scrape_data_nba
 from utils.probability import player_over_probability
 
-app = Flask(__name__)
+import streamlit as st
+import numpy as np
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+data= pd.read_csv('df_sample_data.csv', index_col=0) 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    player_name = request.form['player_name']
-    threshold = int(request.form['threshold'])
-    stat = request.form['stat']
-    last_n_games = int(request.form['last_n_games'])
 
-    df = scrape_data(player_name, last_n_games)
-    probability = player_over_probability(stat, threshold, df)
-
-    return render_template('index.html', prediction_text='The probability of {} getting over {} {} is {:.2f}%'.format(player_name, threshold, stat, probability*100))
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
