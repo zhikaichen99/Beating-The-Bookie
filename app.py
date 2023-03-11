4d8aafe91e04b5b33a6d2f7012a6ed05e49bd432
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-10 20:32:18 -0500

    changed file name

--- app.py
+++ app.py
@@ -7,6 +7,30 @@ import streamlit as st
 import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
+# Thresholds for different categories of stats
+POINTS_THRESHOLDS = [5, 10, 15, 20, 25]
+ASSISTS_THRESHOLDS = [3, 5, 7, 10, 13]
+REBOUNDS_THRESHOLDS = [3, 5, 7, 10, 13]
+THREES_THRESHOLDS = [1, 2, 3, 4, 5]
+
+# Number of last games to consider for analysis
+LAST_N_GAMES = 25
+
+# Players to consider for analysis
+PLAYERS_LIST = ['anthony-edwards']
+
+NCAA_PLAYERS_LIST = ['andrew-funk', 'seth-lundy', 'jalen-pickett', 'camren-wynter',
+                     'chase-audige', 'ty-berry', 'boo-buie', 'matthew-nicholson',
+                     'taylor-hendricks', 'ithiel-horton', 'darius-johnson', 'cj-kelly',
+                     'kendric-davis', 'elijah-mccadden',
+                     'tyrese-proctor', 'jeremy-roach', 'jordan-miller', 'norchard-omier',
+                     'nijel-pack', 'wooga-poplar', 'isaiah-wong', 'jaren-holmes', 'gabe-kalscheur',
+                     'tre-king', 'gradey-dick', 'jalen-wilson', 'isiah-dasher', 'gabe-mcglothan',
+                     'qua-grant', 'donte-powers', 'donald-carey', 'hakim-hart',
+                     'julian-reese', 'donta-scott','jahmir-young', 'trey-galloway',
+                     'miller-kopp', 'race-thompson', 'jermaine-couisnard', 'will-richardson',
+                     'amari-bailey', 'tyger-campbell']
+
 data= pd.read_csv('df_sample_data.csv', index_col=0) 
 
 
