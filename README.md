ce4292d5aa4807a2866c496d63b7ea1427944bf9
zhikaichen99 <zhikaichen1999@gmail.com>
2023-03-11 21:21:00 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Hoop-Probability
+# Beating The Bookie
+
+The objective of this project is to create a predictive model that calculates the probability of a player prop and uses the probabilities to determine whether the prop is worth betting on. Props with a higher probability score will be identified as good bets.
+
+## Project Motivation
+
+The motivation for this project is to develop a tool that can help sports bettors make more informed and strategic decisions when placing bets on player props. Player props are a type of bet that involves wagering on the performance of a specific player, such as the number of points they will score. 
--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import pandas as pd
-import streamlit as st
 
 from utils.sports_bets import basketball
 from utils.scraper import nba_players
+
+import streamlit as st
+import numpy as np
+import string
+
 
 # Set page title, icon, and initial sidebar state
 st.set_page_config(
@@ -10,10 +14,6 @@ st.set_page_config(
     page_icon="📊",
     initial_sidebar_state="expanded"
 )
-
-# Set theme to "light"
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_theme('light')
 
 # Add a title and description to the app
 st.write(
@@ -23,83 +23,72 @@ st.write(
     """
 )
 
-# Create a sidebar with selection boxes for sport, player, and stats
-with st.sidebar:
-    # Sport selection
-    sport = st.selectbox(
-        'Select a sport:',
-        ['NBA', 'NCAA', 'NBL']
-    )
+# define style function to highlight values above 0.92
+def highlight_above_90(val):
+    if float(val) > 90:
+        color = 'green'
+        return f'background-color: {color}'
 
-    # Player selection
-    players = st.multiselect(
-        'Select player(s):',
+
+
+if __name__ == '__main__':
+
+    with st.sidebar:
+
+        sport = st.selectbox(
+        'Which sport do you want to look at',
+        ['NBA', 'NCAA', 'NBL'])
+
+        players = st.multiselect(
+        'Which player(s) do you want to look at',
         nba_players()['Player'].tolist()
-    )
+        )
 
-    # Stat selection
-    stats = st.multiselect(
-        'Select stat(s):',
-        ['Points', 'Assists', 'Rebounds', 'Threes']
-    )
+        stats = st.multiselect(
+            'Which stat(s) do you want to look at',
+            ['Points', 'Assists', 'Rebounds', 'Threes'])
 
-    # Show numeric input fields for each selected stat
-    for stat in stats:
-        if stat == 'Points':
-            points_thresholds = st.slider(
-                'Select a range of points:',
-                min_value=0.0,
-                max_value=30.0,
-                step=0.5,
-                value=(0.0, 30.0)
+        
+        if 'Points' in stats:
+            points_thresholds = st.multiselect(
+                'Points',
+                [num/2 for num in range(0,61)]    
+            )
+        
+        if 'Assists' in stats:
+            assists_thresholds = st.multiselect(
+                'Assists',
+                [num/2 for num in range(0,41)]
+            )
+        
+        if 'Rebounds' in stats:
+            rebounds_thresholds = st.multiselect(
+                'Rebounds',
+                [num/2 for num in range(0,41)]
+            )
+        
+        if 'Threes' in stats:
+            threes_thresholds = st.multiselect(
+                'Threes',
+                [num/2 for num in range(0,11)]
             )
 
-        elif stat == 'Assists':
-            assists_thresholds = st.slider(
-                'Select a range of assists:',
-                min_value=0.0,
-                max_value=20.0,
-                step=0.5,
-                value=(0.0, 20.0)
-            )
+    if len(stats) != 0:
 
-        elif stat == 'Rebounds':
-            rebounds_thresholds = st.slider(
-                'Select a range of rebounds:',
-                min_value=0.0,
-                max_value=20.0,
-                step=0.5,
-                value=(0.0, 20.0)
-            )
+        last_n_games = 25
 
-        elif stat == 'Threes':
-            threes_thresholds = st.slider(
-                'Select a range of threes:',
-                min_value=0.0,
-                max_value=5.0,
-                step=0.5,
-                value=(0.0, 5.0)
-            )
+        player_list = []
+        for player in players:
+            table = str.maketrans("", "", string.punctuation)
+            player = player.translate(table).replace(" ", "-").lower()
+            player_list.append(player)
 
-# Display the results in a data frame
-if len(stats) != 0:
-    last_n_games = 25
+        bets_df = basketball(player_list, last_n_games, points_thresholds, assists_thresholds, rebounds_thresholds, threes_thresholds)
 
-    player_list = []
-    for player in players:
-        player = player.replace(" ", "-").lower()
-        player_list.append(player)
+        # apply style function to probability column
+        styled_df = bets_df.style.applymap(highlight_above_90, subset=pd.IndexSlice[bets_df.index, 'Probability'])
 
-    bets_df = basketball(
-        player_list,
-        last_n_games,
-        points_thresholds,
-        assists_thresholds,
-        rebounds_thresholds,
-        threes_thresholds
-    )
-
-    st.write('## Betting Proposals')
-    st.dataframe(bets_df)
-
-
+        # display styled dataframe in streamlit
+        #st.write(styled_df)
+        
+        st.dataframe(styled_df, use_container_width = True)
